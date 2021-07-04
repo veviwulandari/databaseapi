73f140d16ff26ce20acc37a46ca7dc3d00fa7d6c
veviwulandari <81895811+veviwulandari@users.noreply.github.com>
2021-07-04 23:05:29 +0700

    Add files via upload

---
+++ VeviWulandari_DataBaseAPIProject/app/src/main/java/id/ac/pelitabangsa/veviwulandari_databaseapiproject/MainActivity.java
@@ -0,0 +1,14 @@
+package id.ac.pelitabangsa.veviwulandari_databaseapiproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
